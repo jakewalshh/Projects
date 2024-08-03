7acec191096cead59d0fbd76d72e6ca8f66c51e3
jakewalshh <124164421+jakewalshh@users.noreply.github.com>
2024-08-03 16:53:00 +0000

    Menu pops up

---
+++ PythonPractice/main.py
@@ -0,0 +1,15 @@
+# 03/08/2024        ---  this is small project to  refresh myself with python, 
+# 03/08/2024        ---  it should be a simpple to do list
+
+
+
+
+def main_menu():
+    print("Welcome to the To Do List")
+    print("1. Add Task")
+    print("2. View Task")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+    return choice
+
+main_menu()
